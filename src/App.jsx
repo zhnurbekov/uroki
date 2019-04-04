2cd49be9bfe660a05714f797cd19ad764da2c7ab
zh.nurbekov <n.zhangeldy@gmail.com>
2019-04-05 00:57:19 +0600

    --

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,37 @@ import Lists from './Lists';
 
 class App extends React.Component {
   state = {
-    modalIsOpen: false
+    modalIsOpen: false,
+    users: []
   };
 
   componentWillMount() {
-    console.log('componentWillMount')
+    console.log('componentWillMount');
   }
 
   componentDidMount() {
-    console.log('componentDidMount')
+    fetch('https://reqres.in/api/users?page=2')
+      .then(response => response.json())
+      .then(users => this.setState({ users: users.data }));
+    console.log('componentDidMount');
   }
 
   componentWillUpdate() {
-    console.log('componentWillUpdate')
+    console.log('componentWillUpdate');
   }
 
   componentDidUpdate() {
-    console.log('componentDidUpdate')
+    console.log(this.state.users);
+    console.log('componentDidUpdate');
   }
 
   componentWillUnmount(prevProps, prevState) {
-    console.log('componentWillUnmount')
+    console.log('componentWillUnmount');
   }
 
   shouldComponentUpdate(prevProps) {
-    console.log('shouldComponentUpdate')
-    return true
+    console.log('shouldComponentUpdate');
+    return true;
   }
 
   render() {
@@ -38,8 +43,8 @@ class App extends React.Component {
 
     return (
       <div>
-        {modalIsOpen ? 'модалка открыта' : 'модалка закрыта'} <br />
-        <Lists className="test" changeModal={onChangeModal} />
+        {modalIsOpen ? 'модалка открыта' : 'модалка закрыта'} <br/>
+        <Lists className="test" changeModal={onChangeModal} userList={this.state.users}/>
       </div>
     );
   }
--- src/Lists.jsx
+++ src/Lists.jsx
@@ -3,15 +3,21 @@ import React, { useState } from 'react';
 function Lists(props) {
   // const [modalIsOpen, onChangeModal] = useState(false);
   const [buttonName, changeButtonName] = useState('My Button');
-  const { className, children, changeModal } = props;
+  const { className, changeModal, userList } = props;
+  const listItems = userList.map((user) =>
+    <li key={user.id}>{user.first_name + ' ' + user.last_name}</li>
+  );
+
+
   return (
     <div className={className}>
       <button onClick={() => {
         changeModal();
-        changeButtonName('Changed button name')
+        changeButtonName('Changed button name');
       }}>
         {buttonName}
       </button>
+      <ul>{listItems}</ul>
     </div>
   );
 }
